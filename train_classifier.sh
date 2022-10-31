02bcc9321755fd4233f23963fa64610bd792be4c
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-10-31 13:33:23 +0100

    Support AdaFactor

--- train_classifier.sh
+++ train_classifier.sh
@@ -1,10 +1,13 @@
-MODEL_NAME='t5-base'
+MODEL_NAME='google/t5-v1_1-base'
 BATCH_SIZE=8
 DATASET='eurlex-l1'
 USE_LWAN=false
 SEQ2SEQ=false
 GEN_MAX_LENGTH=32
 TRAINING_MODE='standard'
+OPTIMIZER='adafactor'
+SCHEDULER='constant_with_warmup'
+LEARNING_RATE=1e-4
 export PYTHONPATH=.
 export CUDA_VISIBLE_DEVICES=0
 export TOKENIZERS_PARALLELISM=false
@@ -16,7 +19,7 @@ do
   --seq2seq ${SEQ2SEQ} \
   --use_lwan ${USE_LWAN} \
   --dataset_name ${DATASET} \
-  --output_dir data/logs/${DATASET}/${MODEL_NAME}-${TRAINING_MODE}/seed_${SEED} \
+  --output_dir data/logs/${OPTIMIZER}/${DATASET}/${MODEL_NAME}-${TRAINING_MODE}/seed_${SEED} \
   --max_seq_length 512 \
   --generation_max_length ${GEN_MAX_LENGTH} \
   --do_train \
@@ -30,14 +33,15 @@ do
   --save_strategy epoch \
   --save_total_limit 5 \
   --num_train_epochs 20 \
-  --learning_rate 3e-5 \
   --per_device_train_batch_size ${BATCH_SIZE} \
   --per_device_eval_batch_size ${BATCH_SIZE} \
   --seed ${SEED} \
   --warmup_ratio 0.05 \
   --fp16 \
   --fp16_full_eval \
-  --lr_scheduler_type cosine \
   --gradient_accumulation_steps 1 \
-  --eval_accumulation_steps 1
+  --eval_accumulation_steps 1 \
+  --optim ${OPTIMIZER} \
+  --lr_scheduler_type  ${SCHEDULER} \
+  --learning_rate ${LEARNING_RATE}
 done
