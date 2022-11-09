d28d9f7e1b2a4eddb578540ada1ea67805675e1a
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-11-09 12:54:12 +0100

    Compute mean scores across seeds

--- report_enc2dec_results.py
+++ report_enc2dec_results.py
@@ -2,6 +2,7 @@ import json
 import os
 import argparse
 from data import DATA_DIR
+import numpy as np
 
 
 def main():
@@ -23,15 +24,20 @@ def main():
         print('-' * 100)
         print(f'{f"ENC2DEC-{dec_layers}":<15} | {"VALIDATION":<40} | {"TEST":<40}')
         print('-' * 100)
+        scores = {'eval_micro-f1': [], 'eval_macro-f1': [], 'predict_micro-f1': [], 'predict_macro-f1': []}
         for seed in [21, 32, 42, 84]:
             seed = f'seed_{seed}'
             try:
                 with open(os.path.join(BASE_DIR, seed, 'all_results.json')) as json_file:
                     json_data = json.load(json_file)
                     dev_micro_f1 = float(json_data['eval_micro-f1'])
+                    scores['eval_micro-f1'].append(dev_micro_f1)
                     dev_macro_f1 = float(json_data['eval_macro-f1'])
+                    scores['eval_macro-f1'].append(dev_macro_f1)
                     test_micro_f1 = float(json_data['predict_micro-f1'])
+                    scores['predict_micro-f1'].append(test_micro_f1)
                     test_macro_f1 = float(json_data['predict_macro-f1'])
+                    scores['predict_macro-f1'].append(test_macro_f1)
                     epoch = float(json_data['epoch'])
                 report_line = f'EPOCH: {epoch: 2.1f} | '
                 report_line += f'MICRO-F1: {dev_micro_f1 * 100:.1f}\t'
@@ -42,7 +48,13 @@ def main():
                 print(report_line)
             except:
                 continue
-
+        report_line = f'EPOCH: {"N/A"} | '
+        report_line += f'MICRO-F1: {np.mean(scores["eval_micro-f1"]) * 100:.1f}\t'
+        report_line += f'MACRO-F1: {np.mean(scores["eval_macro-f1"]) * 100:.1f}\t'
+        report_line += ' | '
+        report_line += f'MICRO-F1: {np.mean(scores["predict_micro-f1"]) * 100:.1f}\t'
+        report_line += f'MACRO-F1: {np.mean(scores["predict_macro-f1"]) * 100:.1f}\t'
+        print(report_line)
 
 if __name__ == '__main__':
     main()
