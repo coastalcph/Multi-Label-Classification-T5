ab13c0f5aa997f4d0ffba25c5e7c4338dfd8fa76
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-12-01 12:01:51 +0100

    Support eval mode only + Greedy decoding

--- eval_greedy_search_seq2seq.sh
+++ eval_greedy_search_seq2seq.sh
@@ -1,5 +1,5 @@
 MODEL_NAME='t5-base'
-BATCH_SIZE=16
+BATCH_SIZE=64
 GEN_MAX_LENGTH=128
 TRAINING_MODE='seq2seq'
 OPTIMIZER='adafactor'
@@ -8,7 +8,7 @@ export PYTHONPATH=.
 export CUDA_VISIBLE_DEVICES=7
 export TOKENIZERS_PARALLELISM=false
 
-for DATASET in uklex-l1 eurlex-l1 bioasq-l1 mimic-l1
+for DATASET in uklex-l1
 do
   for SEED in 21 32 42 84
   do
@@ -19,6 +19,8 @@ do
     --dataset_name ${DATASET} \
     --output_dir data/logs/${OPTIMIZER}/${DATASET}/${MODEL_NAME}-${TRAINING_MODE}-original-greedy/fp32/seed_${SEED} \
     --max_seq_length 512 \
+    --metric_for_best_model micro-f1 \
+    --greater_is_better True \
     --n_beams  1 \
     --do_eval \
     --do_pred \
